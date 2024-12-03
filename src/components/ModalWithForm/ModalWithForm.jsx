30a76ad9e7525c4bfae0176cfbb95c07a1b8eb38
Ch-andler <chandlerpedini@gmail.com>
2024-12-03 16:10:47 -0500

    Reviewer changes

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -3,6 +3,8 @@ import "./ModalWithForm.css";
 function ModalWithForm({ children, buttonText, title, activeModal, onClose }) {
   return (
     <div className={`modal ${activeModal === "add-garment" && "modal_opened"}`}>
+      {" "}
+      {/* When I add the "isOpen" in place of the "$" the modal no longer opens. */}
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
         <button
